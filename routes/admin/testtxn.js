25ab703a7500bbc8d97c5f6a1b50741f36c8c0a3
kush aggarwal <kush.aggarwal110085@gmail.com>
2019-08-24 11:01:08 +0530

    Mongodb connection

--- routes/admin/testtxn.js
+++ routes/admin/testtxn.js
@@ -24,7 +24,7 @@ mongoose.connection.on("connected", function() {
 
 // If the connection throws an error
 mongoose.connection.on("error", function(err) {
-  console.log("Mongoose default connection error: " + error);
+  console.log("Mongoose default connection error: " + err);
 });
 
 // When the connection is disconnected
